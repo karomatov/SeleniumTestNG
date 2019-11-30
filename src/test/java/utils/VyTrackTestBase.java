d5cdee7644bf532870f64e1471981cf1be7969c7
khurik87 <khurshed.karomatov@gmail.com>
2019-11-30 11:27:43 -0500

    task 4 test 1 uploaded

--- src/test/java/utils/VyTrackTestBase.java
+++ src/test/java/utils/VyTrackTestBase.java
@@ -1,14 +1,20 @@
 package utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class VyTrackTestBase {
     @BeforeMethod
     public void setUp(){
-        String url = ConfigurationReader.getProperty("url");
+        String url = ConfigurationReader.getProperty("vytrack");
+        String userName = ConfigurationReader.getProperty("user_name");
+        String password = ConfigurationReader.getProperty("password");
         //old version was WebDriver driver = BrowserFactory.getDriver("....") then we called by that obj.
         Driver.get().get(url);
+        Driver.get().findElement(By.name("_username")).sendKeys(userName);
+        Driver.get().findElement(By.name("_password")).sendKeys(password + Keys.ENTER);
     }
 
     @AfterMethod
